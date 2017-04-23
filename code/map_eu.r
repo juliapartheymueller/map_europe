e31f17ccef4e00ed854899d19d01e50f164b2faf
juliapartheymueller <partheym@tmp-119-231.essex.ac.uk>
2017-04-23 13:39:14 +0100

    New version
    Using file.path to allow for use on different platforms

--- code/map_eu.r
+++ code/map_eu.r
@@ -1,16 +1,21 @@
-###############################################################
+###############################################################################################################
 # Mapping data: European countries
-###############################################################
+###############################################################################################################
 library("maptools")
 library("RColorBrewer")
 library("sp")
 library("classInt")
 
 # Set directory
-dir <- "../"
+dir <- ".."
 
-# Read shapes (Retrieved from: http://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts)
-europe.map<-readShapeSpatial(paste0(dir,"data/NUTS_2013_01M_SH/data/NUTS_RG_01M_2013"))
+# Read some data to be plotted
+some.dat <- read.csv(file.path(dir, "data", "Some_data.csv"), sep=";")
+
+# Read shapes
+## Retrieved from: Eurostat
+## http://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts
+europe.map<-readShapeSpatial(file.path(dir, "data", "NUTS_2013_01M_SH", "data", "NUTS_RG_01M_2013"))
 
 # Select shapes by NUTS level
 head(europe.map@data)
@@ -23,22 +28,23 @@ spplot(europe.map2, "NUTS_ID", col.regions=col,
 	main="European Countries",
 	xlim = c(-13, 35), ylim = c(33, 72))
 
-# Read some data to be plotted
-some.dat <- read.csv(paste(dir,"data/Some_data.csv", sep=""), sep=";")
-
 # Match external data by NUTS ID
-europe.map2 <- SpatialPolygonsDataFrame(europe.map2, data= data.frame(europe.map2, some.dat[match(europe.map2$"NUTS_ID", some.dat$"NUTS_ID"),]))
+europe.map2 <- SpatialPolygonsDataFrame(europe.map2,
+	data= data.frame(europe.map2, some.dat[match(europe.map2$"NUTS_ID", some.dat$"NUTS_ID"),]))
+
+# Number of intervals
+num.int <- 7
 
 # Select color scheme
-pal = brewer.pal(7,"Blues")
+pal = brewer.pal(num.int,"Blues")
 
 # Create breaks
 agg.some_data <- aggregate(europe.map2$some_data, by=list(europe.map2$NUTS_ID), FUN=mean)
 summary(agg.some_data$x)
-brks.qt = classIntervals(agg.some_data$x, n = 7, style = "quantile")
-brks.jk = classIntervals(agg.some_data$x, n = 7, style = "jenks")
-brks.eq = classIntervals(agg.some_data$x, n = 7, style = "equal")
-brks.pr = classIntervals(agg.some_data$x, n = 7, style = "pretty")
+brks.qt = classIntervals(agg.some_data$x, n = num.int, style = "quantile")
+brks.jk = classIntervals(agg.some_data$x, n = num.int, style = "jenks")
+brks.eq = classIntervals(agg.some_data$x, n = num.int7, style = "equal")
+brks.pr = classIntervals(agg.some_data$x, n = num.int, style = "pretty")
 
 # Plot
 spplot(europe.map2, c("some_data"), names.attr =c("Some data"),
